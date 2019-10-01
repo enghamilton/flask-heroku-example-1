8d12853a262ea3803bad7ee31a0a7a6f682c1e2a
Hamilton Kamiya <31168921+enghamilton@users.noreply.github.com>
2019-10-01 14:59:53 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,48 @@
 """Flask App Project."""
 
 from flask import Flask, jsonify
+
+import requests
+from lxml import html
+
+USERNAME = "enghamilton"
+PASSWORD = "p4dcGj97"
+
+LOGIN_URL = "https://github.com/login?return_to=%2Fenghamilton"
+URL = "https://github.com/enghamilton?tab=repositories"
+
+def main():
+    #session_requests = requests.session()
+
+    # Create payload
+    payload = {
+        "login": USERNAME, 
+        "password": PASSWORD
+    }
+
+	#https ://pybit.es/requests-session.html
+    with requests.Session() as session:
+		post = session.post(LOGIN_URL, data=payload)
+		r = session.get(URL)		
+		try:
+			#with open(filename) as f:
+			f = open("demofile.txt","w")
+			f.write(r.content)
+		except:
+			print("Something went wrong when writing to the file")
+		finally:
+			f.close()
+		  
+    return (r.content)
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
     """Return homepage."""
-    json_data = {'Hello': 'World!'}
-    return jsonify(json_data)
+    json_data = main()
+    return json_data
 
 
 if __name__ == '__main__':
