15af0939582716cec8f65691eabc62caa2f631cc
Hamilton Kamiya <31168921+enghamilton@users.noreply.github.com>
2019-10-02 12:41:17 -0300

    Update app.py

--- app.py
+++ app.py
@@ -42,9 +42,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """Return homepage."""
-    json_data = {"products":[{"pid":"1","name":"Pizza Portuguesa1","price":"23.50","description":"Queijo mussarela, presunto, ovos, ervilhas, palmito, cebola, oregano, azeitona."},{"pid":"2","name":"pizza 2","price":"23.99","description":"ingrediente 2"},{"pid":"3","name":"pizza 1","price":"23.00","description":"ingrediente 1"},{"pid":"4","name":"pizza 3","price":"12.00","description":"ingrediente da pizza 3"}],"success":1}
-    return jsonify(json_data)
-
+    #json_data = {'"products":[{"pid":"1","name":"Pizza Portuguesa1","price":"23.50","description":"Queijo mussarela, presunto, ovos, ervilhas, palmito, cebola, oregano, azeitona."},{"pid":"2","name":"pizza 2","price":"23.99","description":"ingrediente 2"},{"pid":"3","name":"pizza 1","price":"23.00","description":"ingrediente 1"},{"pid":"4","name":"pizza 3","price":"12.00","description":"ingrediente da pizza 3"}],"success":1'}
+    #return jsonify(json_data)
+    response = make_response(main())
+    response.headers["content-type"] = "text/plain"
+    return response
 
 if __name__ == '__main__':
     app.run()
