fe4167e09175dcd7a7aee80d1b3fd66c1df920a8
Hamilton Kamiya <31168921+enghamilton@users.noreply.github.com>
2019-10-02 12:33:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import requests
 from lxml import html
 
 USERNAME = "enghamilton"
-PASSWORD = p4dcGj97"
+PASSWORD = *******7"
 
 LOGIN_URL = "https://github.com/login?return_to=%2Fenghamilton"
 URL = "https://github.com/enghamilton?tab=repositories"
@@ -42,9 +42,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """Return homepage."""
-    json_data = main()
-    json_data.headers["content-type"] = "text/plain"
-    return json_data
+    json_data = {"products":[{"pid":"1","name":"Pizza Portuguesa1","price":"23.50","description":"Queijo mussarela, presunto, ovos, ervilhas, palmito, cebola, oregano, azeitona."},{"pid":"2","name":"pizza 2","price":"23.99","description":"ingrediente 2"},{"pid":"3","name":"pizza 1","price":"23.00","description":"ingrediente 1"},{"pid":"4","name":"pizza 3","price":"12.00","description":"ingrediente da pizza 3"}],"success":1}
+    return jsonify(json_data)
 
 
 if __name__ == '__main__':
