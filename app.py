c80f7143a2a8a70690bab6ce9fa5d6c998e059ed
Hamilton Kamiya <31168921+enghamilton@users.noreply.github.com>
2019-10-07 19:22:28 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import requests
 from lxml import html
 
 USERNAME = "enghamilton"
-PASSWORD = "p4dcGj97"
+PASSWORD = "*******7"
 
 LOGIN_URL = "https://github.com/login?return_to=%2Fenghamilton"
 URL = "https://github.com/enghamilton?tab=repositories"
@@ -42,11 +42,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """Return homepage."""
-    #json_data = {'"products":[{"pid":"1","name":"Pizza Portuguesa1","price":"23.50","description":"Queijo mussarela, presunto, ovos, ervilhas, palmito, cebola, oregano, azeitona."},{"pid":"2","name":"pizza 2","price":"23.99","description":"ingrediente 2"},{"pid":"3","name":"pizza 1","price":"23.00","description":"ingrediente 1"},{"pid":"4","name":"pizza 3","price":"12.00","description":"ingrediente da pizza 3"}],"success":1'}
-    #return jsonify(json_data)
-    response = make_response(main())
+    json_data = {'"products":[{"pid":"1","name":"Pizza Portuguesa1","price":"23.50","description":"Queijo mussarela, presunto, ovos, ervilhas, palmito, cebola, oregano, azeitona."},{"pid":"2","name":"pizza 2","price":"23.99","description":"ingrediente 2"},{"pid":"3","name":"pizza 1","price":"23.00","description":"ingrediente 1"},{"pid":"4","name":"pizza 3","price":"12.00","description":"ingrediente da pizza 3"}],"success":1'}
+    return jsonify(json_data)
+    
+    '''response = make_response(main())
     response.headers["content-type"] = "text/plain"
-    return response
+    return response'''
 
 if __name__ == '__main__':
     app.run()
