ed6af4fc11f23137d00f48a61f0f039a9750d2a1
Hamilton Kamiya <31168921+enghamilton@users.noreply.github.com>
2019-10-08 16:17:57 -0300

    Update and rename app.py to app-0.py

--- app-0.py
+++ app-0.py
@@ -6,7 +6,7 @@ import requests
 from lxml import html
 
 USERNAME = "enghamilton"
-PASSWORD = "*******7"
+PASSWORD = "p4dcGj97"
 
 LOGIN_URL = "https://github.com/login?return_to=%2Fenghamilton"
 URL = "https://github.com/enghamilton?tab=repositories"
